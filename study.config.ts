313654bdd87a473dc5dcbbc7e07a4fb9c39199d9
r3j0 <readytojoin086@gmail.com>
2026-09-01 20:08:40 +0900

    user: 스터디원 1차 추가

--- study.config.ts
+++ study.config.ts
@@ -9,6 +9,19 @@ export const study = {
       name: "박정근",
       github: "r3j0",
     },
+    {
+      name: "이동현",
+      github: "Dong-H-LEE",
+    },{
+      name: "전병건",
+      github: "wjsqudrjs77",
+    },{
+      name: "이상준",
+      github: "LSJ0352",
+    },{
+      name: "이승종",
+      github: "codingslave99",
+    },
   ],
 } satisfies {
   members: StudyMember[];
