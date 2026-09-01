683d3e13abc4e01206579ed84e2464d0648b32b9
r3j0 <readytojoin086@gmail.com>
2026-09-01 16:27:08 +0900

    feat: enhance Markdown viewer with syntax highlighting

--- src/components/til/MarkdownViewer.tsx
+++ src/components/til/MarkdownViewer.tsx
@@ -1,4 +1,5 @@
 import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
 
 import styles from "./MarkdownViewer.module.css";
@@ -10,7 +11,11 @@ type MarkdownViewerProps = {
 export function MarkdownViewer({ content }: MarkdownViewerProps) {
   return (
     <div className={styles.markdown}>
-      <Markdown remarkPlugins={[remarkGfm]} skipHtml>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[[rehypeHighlight, { detect: false }]]}
+        skipHtml
+      >
         {content}
       </Markdown>
     </div>
